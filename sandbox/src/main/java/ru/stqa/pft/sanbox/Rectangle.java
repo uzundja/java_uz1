f7c9722851a23cecfff6c9199d824e249cd3e038
uzundja <uzundja@mail.ru>
2018-02-04 23:21:35 +0300

    Написан тест

--- sandbox/src/main/java/ru/stqa/pft/sanbox/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Rectangle.java
@@ -7,4 +7,7 @@ public class Rectangle {
     this.a=a;
     this.b=b;
   }
+  public double area (){
+    return this.a * this.b;
+  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sanbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Square.java
@@ -5,4 +5,7 @@ public class Square {
   public Square (double l) {
     this.l=l;
   }
+  public double area () {
+    return this.l * this.l;
+  }
 }
