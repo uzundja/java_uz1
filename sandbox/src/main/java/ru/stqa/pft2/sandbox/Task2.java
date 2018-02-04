8ef3172d10e53c8b57add7685291ca9bdffe8323
uzundja <uzundja@mail.ru>
2018-02-05 00:43:53 +0300

    Вернул обратно расчет distance

--- sandbox/src/main/java/ru/stqa/pft2/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft2/sandbox/Task2.java
@@ -5,7 +5,12 @@ public class Task2 {
     public static void main(String[] args) {
         Point first = new Point(2, 4);
         Point second = new Point(2, 2);
-        double d = distance (second,first);
-        System.out.println("Расстояние между двумя точками а(x1,y1) и b(x2,y2) c координатами x1:" + first.x + " y1:" + first.y + " и x2:" + second.x + " y1:" + second.y + " = " + distance(second,first));
+        double d = distance(first, second);
+
+        System.out.println("Расстояние между двумя точками а(x1,y1) и b(x2,y2) c координатами x1:" + first.x + " y1:" + first.y + " и x2:" + second.x + " y1:" + second.y + " = " + distance(first, second));
+    }
+
+    public static double distance(Point first, Point second) {
+        return Math.sqrt(Math.pow((first.x - second.x), 2) + Math.pow((first.y - second.y), 2));
     }
 }
