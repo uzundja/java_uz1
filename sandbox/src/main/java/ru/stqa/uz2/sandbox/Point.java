0d6fe6d05f988459298212905befdd18f7ad8d96
uzundja <uzundja@mail.ru>
2018-02-04 14:47:53 +0300

    Задание №2

--- sandbox/src/main/java/ru/stqa/uz2/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/uz2/sandbox/Point.java
@@ -1,14 +1,10 @@
 package ru.stqa.uz2.sandbox;
 
 public class Point {
-  public double x1;
-  public double x2;
-  public double y1;
-  public double y2;
-  public Point (double x1,double x2,double y1,double y2){
-    this.x1=x1;
-    this.x2=x2;
-    this.y1=y1;
-    this.y2=y2;
+  public double x;
+  public double y;
+  public Point (double x,double y){
+    this.x=x;
+    this.y=y;
   }
 }
--- sandbox/src/main/java/ru/stqa/uz2/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/uz2/sandbox/Task2.java
@@ -3,12 +3,13 @@ package ru.stqa.uz2.sandbox;
 public class Task2 {
 
     public static void main(String[] args) {
-        Point p = new Point (2,4,2,2);
-        System.out.println("Расстояние между двумя точками а(x1,y1) и b(x2,y2) c координатами x1:" + p.x1 + " y1:" + p.y1 + " и x2:" + p.x2 + " y1:"+ p.y2 + " = "  + distance(p));
-
+        Point first = new Point(2, 4);
+        Point second = new Point(2, 2);
+        double d = distance(first, second);
+        System.out.println("Расстояние между двумя точками а(x1,y1) и b(x2,y2) c координатами x1:" + first.x + " y1:" + first.y + " и x2:" + second.x + " y1:" + second.y + " = " + distance(first, second));
     }
 
-    public static double  distance (Point p) {
-        return Math.sqrt(Math.pow((p.x2 - p.x1), 2) + Math.pow((p.y2 - p.y1), 2));
+    public static double distance(Point first, Point second) {
+        return Math.sqrt(Math.pow((first.x - second.x), 2) + Math.pow((first.y - second.y), 2));
     }
 }
