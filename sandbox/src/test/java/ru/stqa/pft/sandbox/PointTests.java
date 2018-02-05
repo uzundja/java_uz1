14823865639ba74d367e4370c328254cb31a2867
uzundja <uzundja@mail.ru>
2018-02-05 12:24:36 +0300

    Добвален тест на Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,4 +1,16 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
+import ru.stqa.pft2.sandbox.Point;
+import org.testng.annotations.Test;
+
+
 public class PointTests {
+  @Test
+  public void testDistance() {
+    Point p1 = new Point ( 2,2);
+    Point p2 = new Point (4,2);
+
+    Assert.assertEquals(p1.distance(p2), 2.0);
+  }
 }
