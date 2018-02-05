a5265c8ea32add6f99f20f2cd3a455b22594ca18
uzundja <uzundja@mail.ru>
2018-02-05 12:05:05 +0300

    Перенос distance в Point2

--- sandbox/src/main/java/ru/stqa/pft2/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft2/sandbox/Task2.java
@@ -6,7 +6,7 @@ public class Task2 {
         Point p1 = new Point(2, 2);
         Point p2 = new Point (4,2);
 
-        System.out.println("Расстояние между двумя точками а(x1,y1) и b(x2,y2) c координатами x1:" + p1.x + " y1:" + p2.y + " и x2:" + p1.x + " y1:" + p2.y + " = " + p1.distance(p2));
+        System.out.println("Расстояние между двумя точками а(x1,y1) и b(x2,y2) c координатами x1:" + p1.x + " y1:" + p2.y + " и x2:" + p2.x + " y1:" + p2.y + " = " + p1.distance(p2));
     }
 
 }
