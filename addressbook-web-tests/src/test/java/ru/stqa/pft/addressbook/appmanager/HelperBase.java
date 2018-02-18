7a00b9749fbbea78aa1e429adeeabc0d0c3fb76b
uzundja <uzundja@mail.ru>
2018-02-18 18:02:03 +0300

    Реализована двухуровневая архитектура

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelperBase {
+  protected FirefoxDriver wd;
+
+  public HelperBase(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  protected void type(By Locator, String text) {
+    click(Locator);
+    wd.findElement(Locator).clear();
+    wd.findElement(Locator).sendKeys(text);
+  }
+  public boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
