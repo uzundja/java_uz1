47ea17f0ea9bb7beb40ad86124d3679855375fdd
uzundja <uzundja@mail.ru>
2018-02-25 22:33:19 +0300

    Оптимизированные переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -12,14 +12,28 @@ public class NavigationHelper extends HelperBase {
 
   // Идти на страницу Групп
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            &&  wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            &&  isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
+
+
+
   // Идти на страницу Контактов
   public void gotoCreateContact() {
     click(By.linkText("add new"));
   }
+
+
+
   // Открыть страницу Home
   public void gotoHome() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
