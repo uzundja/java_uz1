27e545c7a6321bd449a4c5fa6b5b969d3c7ada65
uzundja <uzundja@mail.ru>
2018-03-10 14:51:52 +0300

    Задание №9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -10,34 +10,6 @@ public class ContactData {
   private final String email;
   private String group;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    ContactData that = (ContactData) o;
-
-    if (id != that.id) return false;
-    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + ( firstname != null ? firstname.hashCode() : 0 );
-    result = 31 * result + ( lastname != null ? lastname.hashCode() : 0 );
-    return result;
-  }
-
-  @Override
-  public String toString() {
-    return "ContactData{" +
-            "id=" + id +
-            ", firstname='" + firstname + '\'' +
-            ", lastname='" + lastname + '\'' +
-            '}';
-  }
 
   public ContactData(int id, String firstname, String middlename, String lastname, String nickname, String address, String email, String group) {
     this.id = id;
@@ -51,8 +23,12 @@ public class ContactData {
   }
 
 
+  public void setId(int id) {
+    this.id = id;
+  }
+
   public ContactData(String firstname, String middlename, String lastname, String nickname, String address, String email, String group) {
-    this.id = Integer.parseInt(null);
+    this.id = 0;
     this.firstname = firstname;
     this.middlename = middlename;
     this.lastname = lastname;
@@ -60,6 +36,7 @@ public class ContactData {
     this.address = address;
     this.email = email;
     this.group = group;
+
   }
 
 
@@ -94,4 +71,35 @@ public class ContactData {
   public String getGroup() {
     return group;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (id != that.id) return false;
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + ( firstname != null ? firstname.hashCode() : 0 );
+    result = 31 * result + ( lastname != null ? lastname.hashCode() : 0 );
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "id=" + id +
+            ", firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
+  }
+
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -2,6 +2,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -9,8 +12,20 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testsContactCreation() {
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().createContact(new ContactData("Антон","Викторович","Торбинский","uzundja","Севастополь","uzundja@mail.ru","test1"),true);
+    ContactData contact = new ContactData("Антон","Викторович","Торбинский","uzundja","Севастополь","uzundja@mail.ru","test1");
+    app.getContactHelper().createContact (contact,true);
     List<ContactData> after= app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size() + 1);
+
+    int max = 0;
+    for (ContactData g : after ) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    contact.setId(max);
+    before.add (contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
   }
 }
